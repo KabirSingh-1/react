9bc5e36fb3e1fbd20dad1152a9bb45a58771b78d
Kabir Singh <kabirsingh44778@gmail.com>
2025-07-14 12:16:22 +0530

    first project commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+import { h1 } from "framer-motion/client";
+
+function App() {
+  return (
+   <h1>chai or react | Kabir Singh</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>chai or code with vite | Kabir Singh</h1>
+  )
+}
+
+export default App
