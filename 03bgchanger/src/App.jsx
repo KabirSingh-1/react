c87d52b1b6d6ceb860a34f8c96c7caea0fb549b8
Kabir Singh <kabirsingh44778@gmail.com>
2025-08-06 14:13:10 +0530

    add bg changer

---
+++ 03bgchanger/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+function App() {
+const [color,setcolor] = useState("olive")
+
+  return (
+    
+     <div class = "w-full h-screen duration-200"
+     style={{backgroundColor:color}}>
+      <div class ="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div class = "flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl" >
+          <button onClick={()=>setcolor("red")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>red</button>
+          <button onClick={()=>setcolor("green")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>green</button>
+          <button onClick={()=>setcolor("white")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"white"}}>white</button>
+          <button onClick={()=>setcolor("yellow")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"yellow"}}>yellow</button>
+          <button onClick={()=>setcolor("pink")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"pink"}}>pink</button>
+          <button onClick={()=>setcolor("black")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"black"}}>black</button>
+          <button onClick={()=>setcolor("blue")} class = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"blue"}}>blue</button>
+      
+        </div>
+      </div>
+     </div>
+  )
+}
+
+export default App
