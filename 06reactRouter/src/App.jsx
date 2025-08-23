3be1d8b1fef1dc2efb698fae1ad60fa417d25f20
KabirSingh-1 <singhkabir44778@gmail.com>
2025-08-23 21:57:59 +0530

    changes

---
+++ 06reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import About from '../components/About'
+import Header from '../components/Header'
+import Home from '../components/Home'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
