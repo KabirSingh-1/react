58d196fade31a55076ac646907b739e164b65733
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-15 12:34:19 +0530

    todo redux

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -1,3 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit';
+// import {configureStore} from '@reduxjs/toolkit';
+// import todoReducer from '../features/todo/todoSlice';
 
-export const store = configureStore({})
+// export const store = configureStore({
+//     reducer: todoReducer
+// })
+
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+
+export const store = configureStore({
+  reducer: {
+    todo: todoReducer, // 👈 give a key for this slice
+  },
+  devTools: process.env.NODE_ENV !== 'production', // enable Redux DevTools
+});
